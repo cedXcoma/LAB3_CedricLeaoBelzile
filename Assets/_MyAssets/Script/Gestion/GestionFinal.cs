9d44340499c4f3b9d6b047f3cdb2f883ac3fa79d
cedXcoma <dr.bidule@outlook.com>
2023-04-11 00:22:39 -0400

    Dodo
    Manque instructions et appuyer

--- Assets/_MyAssets/Script/Gestion/GestionFinal.cs
+++ Assets/_MyAssets/Script/Gestion/GestionFinal.cs
@@ -14,7 +14,7 @@ public class GestionFinal : MonoBehaviour
         _gestionJoueur = FindObjectOfType<Joueur>();
         _gestionJeu = FindObjectOfType<GestionJeu>();
     }
-    private void OnCollisionEnter(Collision collision)
+    /*private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
@@ -67,10 +67,28 @@ public class GestionFinal : MonoBehaviour
                 _gestionJeu.SetNiveau2(_gestionJeu.GetPointage(), Time.time);
                 SceneManager.LoadScene(indexScene + 1);
             }
+    */
 
-
-
-
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")  // Si la collision est produite avec le joueur et la partie n'est pas terminée
+        {
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.green;  // on change la couleur du matériel à vert
+            int noScene = SceneManager.GetActiveScene().buildIndex; // Récupère l'index de la scène en cours
+            if (noScene == (SceneManager.sceneCountInBuildSettings - 2))  // Si nous somme sur le dernier niveau de jeu
+            {
+                _gestionJeu.SetTempsFinal(Time.time);
+                SceneManager.LoadScene(noScene + 1);
+            }
+            else
+            {
+                // Appelle la méthode publique dans gestion jeu pour conserver les informations du niveau 1
+                //_gestionJeu.SetNiveau1(_gestionJeu.GetPointage(), Time.time);
+                // Charge la scène suivante
+                SceneManager.LoadScene(noScene + 1);
+            }
         }
     }
+
 }
+
---
+++ Assets/_MyAssets/Script/Gestion/GestionJeu.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestionJeu : MonoBehaviour
+{
+
+    // Attributs
+    private int _pointage;
+    private float _tempsDepart = 0;
+    private float _tempsFinal = 0;
+    //private int _accrochageNiveau1 = 0;
+    //private int _accrochageNiveau2 = 0;
+    // private float _tempsNiveau1 = 0.0f;
+    //private float _tempsNiveau2 = 0.0f;
+
+
+    private void Awake()
+    {
+        int nbGestionJeu = FindObjectsOfType<GestionJeu>().Length;
+        if (nbGestionJeu > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+        _tempsDepart = Time.time;
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 3 || SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Méthodes publiques
+
+    public void AugmenterPointage()
+    {
+        _pointage++;
+        UIManager uiManager = FindObjectOfType<UIManager>();
+        uiManager.ChangerPointage(_pointage);
+    }
+    public int GetPointage()
+    {
+        return _pointage;
+    }
+
+    public float GetTempsDepart()
+    {
+        return _tempsDepart;
+    }
+
+    public void SetTempsFinal(float p_tempFinal)
+    {
+        _tempsFinal = p_tempFinal - _tempsDepart;
+    }
+
+    public float GetTempsFinal()
+    {
+        return _tempsFinal;
+    }
+
+}
